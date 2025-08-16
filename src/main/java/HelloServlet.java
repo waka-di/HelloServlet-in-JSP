e95c3064743a1e77c1952eed8b65fc0f752655d9
waka-di <miho.1225.miho@icloud.com>
2025-08-16 18:07:52 +0900

    HelloServlet-3失敗

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/HelloServlet")
+@WebServlet("/HelloServlet-3")
 public class HelloServlet extends HttpServlet {
 
     public HelloServlet() {
